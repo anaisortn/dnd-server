f383a278b1aba3ecb39048a7947e9a40151ffb65
anaisortn <anais.ortillon.ao@gmail.com>
2018-05-13 15:16:54 +0200

    cleanup

--- index.js
+++ index.js
@@ -2,12 +2,8 @@
 let app = require('express')()
 let http = require('http').Server(app)
 let io = require('socket.io')(http, {origins: '*:*'})
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
 const PORT = process.env.PORT || 5000
+
 http.listen(PORT, () => console.log('started on port %s', PORT))
 
 // TODO - keep them in the db
